1672ce7a232cc5169a124cccbe6363d34fe1e0d7
DEAN PEGMAN BROWNE <C00272087@ITCARLOW.IE>
2022-09-29 13:01:38 +0100

    START OF LAB 0

---
+++ Lab0/ie.setu.lab0.java
@@ -0,0 +1,10 @@
+//  Name:               Dean Pegman Browne
+//  Student Number:     C00272087
+//  Year:               Year 2
+//  Course:             Software Development KCCOMC_C2P
+
+package ie.setu.OOSP
+
+public class CharTest{
+    
+}
